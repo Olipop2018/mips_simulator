950672119d5fe4f5481be35428f3f07b391842cc
Olivier Innocent <olivier.x2011@hotmail.com>
2019-10-09 16:22:15 -0500

    Update MipsSimulator.py

--- MipsSimulator/MipsSimulator.py
+++ MipsSimulator/MipsSimulator.py
@@ -5,7 +5,6 @@ registers = {"$0": 0, "$8":0,"$9": 0, "$10":0,"$11": 0,
                   "$18":0,"$19": 0, "$20":0,"$21": 0, "$22":0,"$23": 0, "$lo":0,"$hi":0}
 labelIndex = []
 labelName = []
-
 def instrSimulation(instrs):
    pc = int(0)
    bcount=0
@@ -32,14 +31,14 @@ def instrSimulation(instrs):
             print(registers)# print all the registers and their values (testing purposes to see what is happening)
             print(pc)
             print(pcprint)
+
         elif(line[0:3] == "lui"): #lui 
             line = line.replace("lui","")
-            op = '001111'
             line = line.split(",")
-            imm = int(line[1]) if (int(line[1]) > 0 or op == '001000') else (65536 + int(line[2])) # will get the negative or positive inter value. if unsigned and negative will get the unsigned value of th negative integer.
+            imm = int(line[1]) if (int(line[1]) > 0) else (65536 + int(line[2])) # will get the negative or positive inter value. if unsigned and negative will get the unsigned value of th negative integer.
             rd = "$" + str(line[0]) # locate the register in which to write to
             imm = imm << 16
-            registers[rd] = immm 		#Write upper imm to rd designation
+            registers[rd] = immm #Write upper imm to rd designation
             pc += 4# increments pc by 4 
             pcprint = hex(pc)
             print(registers)# print all the registers and their values (testing purposes to see what is happening)
@@ -67,33 +66,6 @@ def instrSimulation(instrs):
             print(pc)
             print(pcprint)
         
-        elif(line[0:2] == "sb"): # sb
-            line = line.replace("sb","")
-            line = line.replace(")","")
-            line = line.replace("(",",")
-            line = line.split(",")
-            if(line[1][0:2]== "0x"):
-                n=16
-            else:
-                n=10
-            imm = format(int(line[1],n),'016b') if (int(line[1],n) > 0) else format(65536 + int(line[1],n),'016b')
-            rs = format(int(line[2]),'05b')
-            rt = format(int(line[0]),'05b')
-            
-       
-        elif(line[0:2] == "lw"): # lw
-            line = line.replace("lw","")
-            line = line.replace(")","")
-            line = line.replace("(",",")
-            line = line.split(",")
-            if(line[1][0:2]== "0x"):
-                n=16
-            else:
-                n=10
-            imm = format(int(line[1],n),'016b') if (int(line[1],n) > 0) else format(65536 + int(line[1],n),'016b')
-            rs = format(int(line[2]),'05b')
-            rt = format(int(line[0]),'05b')
-                
        
         elif(line[0:2] == "sw"): # sw
             line = line.replace("sw","")
@@ -227,6 +199,25 @@ def instrSimulation(instrs):
             print(pc)
             print(pcprint)
 
+        elif(line[0:4] == "slti"): # SLT/U
+            line = line.replace("slti","")
+            if(line[0:1] == "u"):
+               line = line.replace("u","")
+               op = '001011'
+            else:
+                op= '001010'
+            line = line.split(",")
+            imm = int(line[2]) if (int(line[2]) > 0 or op == '001010') else (65536 + int(line[2])) # will get the negative or positive inter value. if unsigned and negative will get the unsigned value of th negative integer.
+            rs = registers[("$" + str(line[1]))] # reads the value from specified register
+            rt = "$" + str(line[0]) # locate the register in which to write to
+            result = rs < imm # does the addition operation
+            registers[rt]= result # writes the value to the register specified
+            pc += 4 # increments pc by 4 
+            pcprint = hex(pc)
+            print(registers)# print all the registers and their values (testing purposes to see what is happening)
+            print(pc)
+            print(pcprint)
+           
         elif(line[0:3] == "xor"): # XOR
             line = line.replace("xor","")
             line = line.split(",")
@@ -235,6 +226,34 @@ def instrSimulation(instrs):
             rt = registers[("$" + str(line[2]))]
             result = rs ^ rt # does the addition operation
             registers[rd]= result
+            pc+= 4 # increments pc by 4 
+            pcprint =  hex(pc)
+            print(registers)# print all the registers and their values (testing purposes to see what is happening)
+            print(pc)
+            print(pcprint)
+        
+        elif(line[0:3] == "ori"): # ori
+            line = line.replace("ori","")
+            line = line.split(",")
+            imm = int(line[2]) if (int(line[2]) > 0) else (65536 + int(line[2])) # will get the negative or positive inter value. if unsigned and negative will get the unsigned value of th negative integer.
+            rs = registers[("$" + str(line[1]))] # reads the value from specified register
+            rt = "$" + str(line[0]) # locate the register in which to write to
+            result = rs | imm # does the addition operation
+            registers[rt]= result # writes the value to the register specified
+            pc+= 4 # increments pc by 4 
+            pcprint =  hex(pc)
+            print(registers)# print all the registers and their values (testing purposes to see what is happening)
+            print(pc)
+            print(pcprint)
+            
+        elif(line[0:4] == "andi"): # andi
+            line = line.replace("andi","")
+            line = line.split(",")
+            imm = int(line[2]) if (int(line[2]) > 0) else (65536 + int(line[2])) # will get the negative or positive inter value. if unsigned and negative will get the unsigned value of th negative integer.
+            rs = registers[("$" + str(line[1]))] # reads the value from specified register
+            rt = "$" + str(line[0]) # locate the register in which to write to
+            result = rs & imm # does the addition operation
+            registers[rt]= result # writes the value to the register specified
             pc+= 4 # increments pc by 4 
             pcprint =  hex(pc)
             print(registers)# print all the registers and their values (testing purposes to see what is happening)
@@ -262,53 +281,7 @@ def instrSimulation(instrs):
                         hexstr= hex(int(hexstr[0], 2))
                         f.write(hexstr+ '\n')#str('000010') + str(format(int(labelIndex[i]),'026b')) + '\n'+ hexstr+ '\n')
 
-        elif(line[0:6] == "ori"): # ORI 
-            line = line.replace("ori","")
-            op = '001101'
-            line = line.split(",")
-            imm = int(line[2]) if (int(line[2]) > 0 or op == '001000') else (65536 + int(line[2])) # will get the negative or positive inter value. if unsigned and nega			tive will get the unsigned value of th negative integer.
-            rs = registers[("$" + str(line[1]))] # reads the value from specified register
-            rt = "$" + str(line[0]) # locate the register in which to write to
-            result = rs | imm # does the or imm operation
-            registers[rt]= result # writes the value to the register specified
-            pc += 4# increments pc by 4 
-            pcprint = hex(pc)
-            print(registers)# print all the registers and their values (testing purposes to see what is happening)
-            print(pc)
-            print(pcprint)
 
-        elif(line[0:6] == "sltiu"): # SLTIU 
-            line = line.replace("sltiu","")
-            op = '001011'
-            line = line.split(",")
-            imm = int(line[2]) if (int(line[2]) > 0 or op == '001000') else (65536 + int(line[2])) # will get the negative or positive inter value. if unsigned and nega			tive will get the unsigned value of th negative integer.
-            rs = registers[("$" + str(line[1]))] # reads the value from specified register
-            rt = "$" + str(line[0]) # locate the register in which to write to
-            if(rs < imm): 
-                registers[rt]= 1 # writes the value to the register specified
-            else:
-                registers[rt]= 0
-            pc += 4# increments pc by 4 
-            pcprint = hex(pc)
-            print(registers)# print all the registers and their values (testing purposes to see what is happening)
-            print(pc)
-            print(pcprint)
-
-        elif(line[0:6] == "andi"): # ANDI 
-            line = line.replace("andi","")
-            op = '001100'
-            line = line.split(",")
-            imm = int(line[2]) if (int(line[2]) > 0 or op == '001000') else (65536 + int(line[2])) # will get the negative or positive inter value. if unsigned and nega			tive will get the unsigned value of th negative integer.
-            rs = registers[("$" + str(line[1]))] # reads the value from specified register
-            rt = "$" + str(line[0]) # locate the register in which to write to
-            result = rs & imm # does the and operation
-            registers[rt]= result # writes the value to the register specified
-            pc += 4# increments pc by 4 
-            pcprint = hex(pc)
-            print(registers)# print all the registers and their values (testing purposes to see what is happening)
-            print(pc)
-            print(pcprint)
-            f.write(hexstr+ '\n')#str('000010') + str(format(int(labelIndex[i]),'026b')) + '\n'+ hexstr+ '\n')
 
 
 
