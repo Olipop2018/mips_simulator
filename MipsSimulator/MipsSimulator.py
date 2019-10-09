dc3cc8452e2c07c915fcd94fcd46a7cfb69a394b
Olivier Innocent <olivier.x2011@hotmail.com>
2019-10-09 00:19:00 -0500

    Update MipsSimulator.py

--- MipsSimulator/MipsSimulator.py
+++ MipsSimulator/MipsSimulator.py
@@ -1,40 +1,18 @@
-def saveJumpLabel(asm,labelIndex, labelName):
-    lineCount = 0
-    for line in asm:
-        line = line.replace(" ","")
-        if(line.count(":")):
-            labelName.append(line[0:line.index(":")]) # append the label name
-            labelIndex.append(lineCount) # append the label's index
-            asm[lineCount] = line[line.index(":")+1:]
-        lineCount += 1
-    for item in range(asm.count('\n')): # Remove all empty lines '\n'
-        asm.remove('\n')
-
-def main():
-    labelIndex = []
-    labelName = []
-    f = open("mc.txt","w+")
-    h = open("mips.asm","r")
-    asm = h.readlines()
-    pc = 0
-    memory = [0] *4096 #Remember when ever you get an address in hex subtract 8192 from it then write to it
-    DIC = 0				#Dynamic Instruction Count
-    registers = {"$0": 0, "$8":0,"$9": 0, "$10":0,"$11": 0, 
+memory = [0] *4096 #Remember when ever you get an address in hex subtract 8192 from it then write to it
+DIC = 0				#Dynamic Instruction Count
+registers = {"$0": 0, "$8":0,"$9": 0, "$10":0,"$11": 0, 
                   "$12":0,"$13": 0, "$14":0,"$15": 0, "$16":0,"$17": 0, 
                   "$18":0,"$19": 0, "$20":0,"$21": 0, "$22":0,"$23": 0, "$lo":0,"$hi":0}
-
-    for item in range(asm.count('\n')): # Remove all empty lines '\n'
-        asm.remove('\n')
-
-    saveJumpLabel(asm,labelIndex,labelName) # Save all jump's destinations
-    bcount=0
-    for line in asm:
-        line = line.replace("\n","") # Removes extra chars
-        line = line.replace("$","")
-        line = line.replace(" ","")
-        line = line.replace("zero","0") # assembly can also use both $zero and $0
+labelIndex = []
+labelName = []
+def instrSimulation(instrs):
+   pc = int(0)
+   bcount=0
+   while True:
         bcount+=1
-		
+        if (int(pc/4) > len(instrs)):
+            return
+        line = instrs[int(pc/4)]
         if(line[0:4] == "addi"): # ADDI/U 
             line = line.replace("addi","")
             if(line[0:1] == "u"):
@@ -53,14 +31,13 @@ def main():
             print(registers)# print all the registers and their values (testing purposes to see what is happening)
             print(pc)
             print(pcprint)
-
-	elif(line[0:3] == "lui"): #lui 
+        elif(line[0:3] == "lui"): #lui 
             line = line.replace("lui","")
             op = '001111'
             line = line.split(",")
             imm = int(line[1]) if (int(line[1]) > 0 or op == '001000') else (65536 + int(line[2])) # will get the negative or positive inter value. if unsigned and negative will get the unsigned value of th negative integer.
             rd = "$" + str(line[0]) # locate the register in which to write to
-			imm = imm << 16
+            imm = imm << 16
             registers[rd] = immm 		#Write upper imm to rd designation
             pc += 4# increments pc by 4 
             pcprint = hex(pc)
@@ -80,12 +57,12 @@ def main():
             imm = format(int(line[1],n),'016b')
             rs = int(registers[("$" + str(line[2]))])
             rt = registers[("$" + str(line[0]))]
-			mem = imm + rs
+            mem = imm + rs
             mem = mem - int('0x2000', 16)
-			register[rt] = memory[mem]
+            register[rt] = memory[mem]
             pc += 4# increments pc by 4 
             pcprint = hex(pc)
-			print(registers)# print all the registers and their values (testing purposes to see what is happening)
+            print(registers)# print all the registers and their values (testing purposes to see what is happening)
             print(pc)
             print(pcprint)
         
@@ -150,6 +127,7 @@ def main():
             if(rs == rt):
                  pc+= 4
                  pc+= (imm << 2)
+                 bcount+=1
             else:
                 pc+= 4
             pcprint=  hex(pc)
@@ -169,6 +147,7 @@ def main():
             if(rs != rt):
                  pc+= 4
                  pc+= (imm << 2)
+                 bcount=0
             else:
                 pc+= 4
             pcprint=  hex(pc)
@@ -176,14 +155,6 @@ def main():
             print(pc)
             print(pcprint)
 
-        elif(line[0:3] == "add"): # ADD
-            line = line.replace("add","")
-            line = line.split(",")
-            rd = format(int(line[0]),'05b')
-            rs = format(int(line[1]),'05b')
-            rt = format(int(line[2]),'05b')
-          
-            
         elif(line[0:3] == "srl"): # SRL
             line = line.replace("srl","")
             line = line.split(",")
@@ -210,20 +181,7 @@ def main():
             pcprint =  hex(pc)
             print(registers)# print all the registers and their values (testing purposes to see what is happening)
             print(pc)
-            print(pcprint)
-                       
-        elif(line[0:3] == "slt"): # SLT/U
-            line = line.replace("slt","")
-            if(line[0:1] == "u"):
-               line = line.replace("u","")
-               op = '001001'
-            else:
-                op= '101010'
-            line = line.split(",")
-            rd = format(int(line[0]),'05b')
-            rs = format(int(line[1]),'05b')
-            rt = format(int(line[2]),'05b')
-            
+            print(pcprint)     
             
         elif(line[0:4] == "mult"): # MULT/U
             line = line.replace("mult","")
@@ -235,38 +193,36 @@ def main():
             line = line.split(",")
             rs = registers[("$" + str(line[0]))]	#First register
             rt = registers[("$" + str(line[1]))]	#Second register
-			
-			temp = rs * rt	#Multiply
-			registers[{"$hi"}] = temp << 32		#Shift high right 32
-			registers[{"$hi"}] = registers[{"$hi"}] >> 32	#Shift back 32
-			registers[{"$lo"}] = temp >> 32	#Shift low left 32
-			
-			pc += 4# increments pc by 4 
-			pcprint =  hex(pc)
-			print(registers)# print all the registers and their values (testing purposes to see what is happening)
+            temp = rs * rt	#Multiply
+            registers[{"$hi"}] = temp << 32		#Shift high right 32
+            registers[{"$hi"}] = registers[{"$hi"}] >> 32	#Shift back 32
+            registers[{"$lo"}] = temp >> 32	#Shift low left 32
+            pc += 4# increments pc by 4 
+            pcprint =  hex(pc)
+            print(registers)# print all the registers and their values (testing purposes to see what is happening)
             print(pc)
             print(pcprint)
-			
-		elif(line[0:4] == "mflo"): #MFLO
+
+        elif(line[0:4] == "mflo"): #MFLO
             line = line.replace("mflo","")
             op = '001010'
             line = line.split(",")
             rs = "$" + str(line[0])		#Register to write to
-			registers[rs] = registers[{"$lo"}]	#Write value to register
-			pc += 4# increments pc by 4 
-			pcprint =  hex(pc)
-			print(registers)# print all the registers and their values (testing purposes to see what is happening)
+            registers[rs] = registers[{"$lo"}]	#Write value to register
+            pc += 4# increments pc by 4 
+            pcprint =  hex(pc)
+            print(registers)# print all the registers and their values (testing purposes to see what is happening)
             print(pc)
             print(pcprint)
-			
-		elif(line[0:4] == "mfhi"): #MFHI
+
+        elif(line[0:4] == "mfhi"): #MFHI
             line = line.replace("mfhi","")
-			op = '001000'
+            op = '001000'
             line = line.split(",")
             rd = "$" + str(line[0])		#Register to write to
-			registers[rd] = registers[{"$hi"}]	#Write value to register
-			pc += 4# increments pc by 4 
-			print(registers)# print all the registers and their values (testing purposes to see what is happening)
+            registers[rd] = registers[{"$hi"}]	#Write value to register
+            pc += 4# increments pc by 4 
+            print(registers)# print all the registers and their values (testing purposes to see what is happening)
             print(pc)
             print(pcprint)
 
@@ -351,6 +307,41 @@ def main():
             print(registers)# print all the registers and their values (testing purposes to see what is happening)
             print(pc)
             print(pcprint)
+            f.write(hexstr+ '\n')#str('000010') + str(format(int(labelIndex[i]),'026b')) + '\n'+ hexstr+ '\n')
+
+
+
+def saveJumpLabel(asm,labelIndex, labelName):
+    lineCount = 0
+    for line in asm:
+        line = line.replace(" ","")
+        if(line.count(":")):
+            labelName.append(line[0:line.index(":")]) # append the label name
+            labelIndex.append(lineCount) # append the label's index
+            asm[lineCount] = line[line.index(":")+1:]
+        lineCount += 1
+    for item in range(asm.count('\n')): # Remove all empty lines '\n'
+        asm.remove('\n')
+
+def main():
+    labelIndex = []
+    labelName = []
+    f = open("mc.txt","w+")
+    h = open("mips.asm","r")
+    asm = h.readlines()
+    instrs = []
+    for item in range(asm.count('\n')): # Remove all empty lines '\n'
+        asm.remove('\n')
+
+    saveJumpLabel(asm,labelIndex,labelName) # Save all jump's destinations
+    for line in asm:
+        line = line.replace("\n","") # Removes extra chars
+        line = line.replace("$","")
+        line = line.replace(" ","")
+        line = line.replace("zero","0") # assembly can also use both $zero and $0
+        instrs.append(line)
+    
+    instrSimulation(instrs)
 
 
 
